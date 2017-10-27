ba5168d769442be64ac863db7738816cec97973e
3sho7mi8 <hiro.pinot@gmail.com>
2017-10-28 07:31:45 +0900

    Add Scrapingon

--- lib/scrapingon.rb
+++ lib/scrapingon.rb
@@ -4,12 +4,16 @@ class Scrapingon
 
   attr_accessor :url
 
-  def initialize(url)
+  def initialize(url, id)
     @url = url
     @html = open(@url, "r:binary", "User-Agent" => "Googlebot/2.1").read
     @doc = Nokogiri::HTML(@html, nil, "utf-8")
+    @id = id
   end
 
-  def image
-  end
+  class_eval <<-EOS
+    def title
+      #{Condition.first.get_title}
+    end
+  EOS
 end
